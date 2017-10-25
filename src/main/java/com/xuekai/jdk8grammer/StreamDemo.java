069b671c87832de8433c7ea00b37f65056e0cd2b
shixuekai <shixuekai@meituan.com>
2017-10-25 20:16:32 +0800

    测试jdk8语法，stream

---
+++ src/main/java/com/xuekai/jdk8grammer/StreamDemo.java
@@ -0,0 +1,34 @@
+package com.xuekai.jdk8grammer;
+
+import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.ListeningExecutorService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * stream的特性示例
+ */
+public class StreamDemo {
+
+    /**
+     * 我们可以将stream认为是Iterator的高级应用
+     *
+     *
+     */
+    public static void main(String[] args) {
+
+        List<Integer> nums= Lists.newArrayList(1,null,4,null,6,9);
+        List<Integer> list=nums.stream().filter(a->a!=null).collect(Collectors.toList());
+
+        list.forEach(a-> System.out.println(a));
+
+        list.forEach(a->{
+            int i=4;
+            if(a!=i){
+                System.out.println(a);
+            }
+        });
+    }
+}
